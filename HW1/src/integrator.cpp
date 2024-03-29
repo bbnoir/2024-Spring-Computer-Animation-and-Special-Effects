38fa20774d7708c6377f274196f9d0278ddf772b
Yu-Sheng Yang <bbnoir83@gmail.com>
2024-03-29 16:08:26 +0800

    HW1 - init

---
+++ HW1/src/integrator.cpp
@@ -0,0 +1,44 @@
+#include "integrator.h"
+
+#include "configs.h"
+
+void ExplicitEuler::integrate(const std::vector<Particles *> &particles, std::function<void(void)>) const {
+  // TODO: Integrate velocity and acceleration
+  //   1. Integrate velocity.
+  //   2. Integrate acceleration.
+  //   3. You should not compute position using acceleration. Since some part only update velocity. (e.g. impulse)
+  // Note:
+  //   1. You don't need the simulation function in explicit euler.
+  //   2. You should do this first because it is very simple. Then you can chech your collision is correct or not.
+  //   3. This can be done in 5 lines. (Hint: You can add / multiply all particles at once since it is a large matrix.)
+}
+
+void ImplicitEuler::integrate(const std::vector<Particles *> &particles,
+                              std::function<void(void)> simulateOneStep) const {
+  // TODO: Integrate velocity and acceleration
+  //   1. Backup original particles' data.
+  //   2. Integrate velocity and acceleration using explicit euler to get Xn+1.
+  //   3. Compute refined Xn+1 using (1.) and (2.).
+  // Note:
+  //   1. Use simulateOneStep with modified position and velocity to get Xn+1.
+}
+
+void MidpointEuler::integrate(const std::vector<Particles *> &particles,
+                              std::function<void(void)> simulateOneStep) const {
+  // TODO: Integrate velocity and acceleration
+  //   1. Backup original particles' data.
+  //   2. Integrate velocity and acceleration using explicit euler to get Xn+1.
+  //   3. Compute refined Xn+1 using (1.) and (2.).
+  // Note:
+  //   1. Use simulateOneStep with modified position and velocity to get Xn+1.
+}
+
+void RungeKuttaFourth::integrate(const std::vector<Particles *> &particles,
+                                 std::function<void(void)> simulateOneStep) const {
+  // TODO: Integrate velocity and acceleration
+  //   1. Backup original particles' data.
+  //   2. Compute k1, k2, k3, k4
+  //   3. Compute refined Xn+1 using (1.) and (2.).
+  // Note:
+  //   1. Use simulateOneStep with modified position and velocity to get Xn+1.
+}
