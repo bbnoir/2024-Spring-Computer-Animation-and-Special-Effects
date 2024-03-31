e6dbfb2633d0187803b2c8d32822a08d51aea0d9
Yu-Sheng Yang <bbnoir83@gmail.com>
2024-03-31 16:06:37 +0800

    HW1 - finish RungeKutta4

--- HW1/src/integrator.cpp
+++ HW1/src/integrator.cpp
@@ -73,4 +73,34 @@ void RungeKuttaFourth::integrate(const std::vector<Particles *> &particles,
   //   3. Compute refined Xn+1 using (1.) and (2.).
   // Note:
   //   1. Use simulateOneStep with modified position and velocity to get Xn+1.
+  std::vector<Eigen::Matrix4Xf> oriPositions, oriVelocitys, k1, k2, k3, k4, l1, l2, l3, l4;
+  for (int i = 0; i < particles.size(); i++) {
+    oriPositions.push_back(particles[i]->position());
+    oriVelocitys.push_back(particles[i]->velocity());
+    k1.push_back(particles[i]->velocity() * deltaTime);
+    l1.push_back(particles[i]->acceleration() * deltaTime);
+    particles[i]->position() = oriPositions[i] + k1[i] * 0.5f;
+    particles[i]->velocity() = oriVelocitys[i] + l1[i] * 0.5f;
+  }
+  simulateOneStep();
+  for (int i = 0; i < particles.size(); i++) {
+    k2.push_back(particles[i]->velocity() * deltaTime);
+    l2.push_back(particles[i]->acceleration() * deltaTime);
+    particles[i]->position() = oriPositions[i] + k2[i] * 0.5f;
+    particles[i]->velocity() = oriVelocitys[i] + l2[i] * 0.5f;
+  }
+  simulateOneStep();
+  for (int i = 0; i < particles.size(); i++) {
+    k3.push_back(particles[i]->velocity() * deltaTime);
+    l3.push_back(particles[i]->acceleration() * deltaTime);
+    particles[i]->position() = oriPositions[i] + k3[i];
+    particles[i]->velocity() = oriVelocitys[i] + l3[i];
+  }
+  simulateOneStep();
+  for (int i = 0; i < particles.size(); i++) {
+    k4.push_back(particles[i]->velocity() * deltaTime);
+    l4.push_back(particles[i]->acceleration() * deltaTime);
+    particles[i]->position() = oriPositions[i] + (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6.0f;
+    particles[i]->velocity() = oriVelocitys[i] + (l1[i] + 2 * l2[i] + 2 * l3[i] + l4[i]) / 6.0f;
+  }
 }
