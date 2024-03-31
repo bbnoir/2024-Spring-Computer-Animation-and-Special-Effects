bdfe2916cb89f5adbbeb4186989eee561e31f3b0
Yu-Sheng Yang <bbnoir83@gmail.com>
2024-03-31 14:33:46 +0800

    HW1 - finish Expicit Euler method

--- HW1/src/integrator.cpp
+++ HW1/src/integrator.cpp
@@ -11,6 +11,10 @@ void ExplicitEuler::integrate(const std::vector<Particles *> &particles, std::fu
   //   1. You don't need the simulation function in explicit euler.
   //   2. You should do this first because it is very simple. Then you can chech your collision is correct or not.
   //   3. This can be done in 5 lines. (Hint: You can add / multiply all particles at once since it is a large matrix.)
+  for (auto &particle : particles) {
+    particle->velocity() += particle->acceleration() * deltaTime;
+    particle->position() += particle->velocity() * deltaTime;
+  }
 }
 
 void ImplicitEuler::integrate(const std::vector<Particles *> &particles,
